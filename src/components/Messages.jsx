202b96895c472795a5facc5e33bf06f70ab3dffa
Jip Fr <jipfrijlink@gmail.com>
2021-08-22 13:49:23 +0200

    feat: add better reply style

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -10,6 +10,11 @@ export function Message(props) {
 	return (
 		<div tabrow="true">
 			<div className="is-message border-t border-border normal-case p-1 px-2" tabcol="true">
+				{props.msg.referenced_message ? <div className="flex space-x-2 items-center truncate" tabcol="true">
+					<img src="/silk/icons/arrow_turn_right.png" alt="" />
+					<span>{props.msg.referenced_message.author.username}:</span>
+					<span>{props.msg.referenced_message.content}</span>
+				</div> : ''}
 				<p><strong>{props.name}</strong>{props.subtitle ? <span className="ml-3">{props.subtitle}</span> : ''}</p>
 				<div>{props.children}</div>
 			</div>
