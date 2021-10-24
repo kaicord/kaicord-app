108f1c4844467672dfc8a7c492ee89e1c51a40d5
Jip Fr <jipfrijlink@gmail.com>
2021-10-24 16:07:15 +0200

    fix(guilds): fix unnamed folders not showing up

--- src/views/Guilds.jsx
+++ src/views/Guilds.jsx
@@ -9,11 +9,12 @@ export function Guilds(props) {
 			guilds: folder.guild_ids.map(id => props.guilds.find(g => g.getID() === id)).filter(Boolean)
 		}
 	})
+
 	return (
 		<div>
 			{groups.map(group => {
 				return (
-					<List collapsible={group.guilds.length > 1} title={group.guilds.length > 1 ? group.name : undefined} key={group.guild_ids.join(',')}>
+					<List collapsible={group.guilds.length > 1} title={group.guilds.length > 1 ? (group.name || group.guilds.map(g => g.getName()).join(', ')) : undefined} key={group.guild_ids.join(',')}>
 						{group.guilds.map(guild => {
 							return (
 								<ListItemWithArrow to={`/guild/${guild.getID()}`} key={guild.getID()} img={guild.getIconUrl()}>
